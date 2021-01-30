d306630b7c38f898fe917a1df4b491e773e4c579
Lalaina;2002 <rivo2302@gmail.com>
2021-01-30 21:33:44 +0300

    Gestiondes conflits

--- models/connectDB.php
+++ models/connectDB.php
@@ -1,17 +1,22 @@
 <?php
-    function dbConnect(){
-        // Les configurations de la base de donnée
-        define('DB_HOST', 'localhost');
-        define('DB_USER', 'estics');
-        define('DB_PASS', '__estics__');
-        define('DB_NAME', 'ESTICS');
-        
-        try{
-            $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-            // Si connection non établie
-            return $pdo ;
-        } catch(Exception $error){
-            die("[ERREUR] Connexion à la BDD a échouée :" . $error->getMessage());
+
+    class CONNECT_BDD{
+        public function dbConnect(){
+            // Les configurations de la base de donnée
+            define('DB_HOST', 'localhost');
+            define('DB_USER', 'root');
+            define('DB_PASS', '');
+            define('DB_NAME', 'note_bdd');
+            
+            try{
+                $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+                // Si connection non établie
+                return $pdo ;
+            } catch(Exception $error){
+                die("[ERREUR] Connexion à la BDD a échouée :" . $error->getMessage());
+            }
+
         }
     }
+
 ?>
